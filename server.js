1a3b2e8a4fb9eb86734d75f34f4bafa1455d9bcf
An Dong <hoangdonghvcs@gmail.com>
2016-11-16 13:58:07 +0000

    modified index.html, server.js

--- server.js
+++ server.js
@@ -1,10 +1,58 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// app.get('/', function (req, res) {
+//   res.send('Hello World! \n My name is Dong');
+// });
+
+// app.listen(8080, function () {
+//   console.log('Example app listening on port 8080!')
+// })
+
+var server = require('express');
+var app = server();
+var moment = require('moment');
+var fs = require('fs');
+var path = require('path');
+
+var port = process.env.PORT || 3500;
+
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.get('/', function(req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
+});
